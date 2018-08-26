8067ae6d56b74982825aeb1308347c401406fe4e
Sanjeet <sanjeet.kumar@zylotech.com>
2018-08-26 17:45:57 +0530

    wip

---
+++ client/app/constants/ServerUrl.js
@@ -0,0 +1,17 @@
+let baseURL = '';
+let socketURL = '';
+
+if (process.env.NODE_ENV === 'production' ) {
+  // console.log('Production...');
+  baseURL = '';
+  socketURL = '';
+} else {
+  // console.log('Devlopment...');
+  baseURL = 'http://localhost:3100/api';
+  socketURL = 'http://localhost:3100';
+
+}
+
+export const BASE_URL = baseURL;
+export const SOCKET_URL = socketURL;
+
